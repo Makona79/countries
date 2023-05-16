1eb5317bb0170641cbabb06d030c343150e6c9ae
Evgenii Kalukov <makona79@yandex.ru>
2023-05-16 13:40:32 +0400

    add Homepage

--- Countries-API/src/App.tsx
+++ Countries-API/src/App.tsx
@@ -1,59 +1,26 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import Header from './components/Header'
 import './App.css'
-import Controls from './components/Controls'
 import Main from './components/Main'
-import axios from 'axios'
-import { ALL_COUNTRIES } from './config'
-import List from './components/List'
-import Card from './components/Card'
-
-type CountriesType = {
-	flags: any,
-	name: string,
-	population: string,
-	region: string,
-	capital: string
-}
+import Homepage, { CountriesType } from './pages/Homepage';
+import Detailes from './pages/Detailes';
+import NotFound from './pages/NotFound';
 
 function App() {
 	const [countries, setCountries] = useState<CountriesType[]>([]);
 	console.log(countries);
 
-	useEffect(() => {
-		axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data))
-	}, [])
-
 	return (
 		<>
 			<Header />
 			<Main>
-				<Controls />
-				<List>
-					{countries.map((c) => {
-						const countryInfo = {
-							img: c.flags.png,
-							name: c.name,
-							info: [
-								{
-									title: 'Population',
-									description: c.population.toLocaleString()
-								},
-								{
-									title: 'Region',
-									description: c.region
-								},
-								{
-									title: 'Capital',
-									description: c.capital
-								},
-							]
-						};
-						return (
-							<Card key={c.name} {...countryInfo} />
-						)
-					})}
-				</List>
+				<Routes>
+					<Route path="/" element={<Homepage countries={countries} setCountries={setCountries} />} />
+					<Route path="/country/:name" element={<Detailes />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+				
 			</Main>
 		</>
 	)
--- Countries-API/src/pages/Homepage.tsx
+++ Countries-API/src/pages/Homepage.tsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+import { ALL_COUNTRIES } from '../config'
+import List from '../components/List'
+import Card from '../components/Card'
+import Controls from '../components/Controls'
 
-const Homepage = () => {
+export type CountriesType = {
+	flags: any,
+	name: string,
+	population: string,
+	region: string,
+	capital: string
+}
+// type HomePageProps = {
+// 	countries:CountriesType,
+
+// }
+
+const Homepage = ({ countries, setCountries }: any) => {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!countries.length)
+			axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data))
+	}, [])
 	return (
-		<div>Homepage</div>
+		<>
+			<Controls />
+			<List>
+				{countries.map((c: any) => {
+					const countryInfo = {
+						img: c.flags.png,
+						name: c.name,
+						info: [
+							{
+								title: 'Population',
+								description: c.population.toLocaleString()
+							},
+							{
+								title: 'Region',
+								description: c.region
+							},
+							{
+								title: 'Capital',
+								description: c.capital
+							},
+						]
+					};
+					return (
+						<Card key={c.name} {...countryInfo} onClick={() => navigate(`/country/${c.name}`)} />
+					)
+				})}
+			</List>
+		</>
 	)
 }
 
